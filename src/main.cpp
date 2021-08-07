2129bc36d58780fcd612e0a363f44868c8f3923c
Kayyum Uddin <48589486+kayyumuddin@users.noreply.github.com>
2021-08-07 23:24:35 +0530

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,58 @@
 #include <NewPing.h>
 #include <SimpleTimer.h>
 
+#define GND_PIN 7
+#define VCC_PIN 6
+
+#define LED_RED 9
+#define LED_GREEN 10
+#define RELAY_PIN 11
+
+#define TRIGGER_PIN 5   
+#define ECHO_PIN 4    
+#define MAX_DISTANCE 40 
+
 SimpleTimer timer;
 
-bool switchVal;
+bool sonarState;
 
-const int ledPin = 13; 
-
-// Variables will change:
-int ledState = HIGH;       // the current state of the output pin
+int ledState = LOW;       // the current state of the output pin
 int buttonState;           // the current reading from the input pin
 int lastButtonState = LOW; // the previous reading from the input pin
-
 
 unsigned long lastDebounceTime = 0; 
 unsigned long debounceDelay = 100;   
 
-#define TRIGGER_PIN 10   
-#define ECHO_PIN 9       
-#define MAX_DISTANCE 40 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
 
-void take_data()
+void takeData()
 {
   int distance = sonar.ping_cm();
   if (distance > 5 && distance < 30)
   {
-    switchVal = HIGH;
+    sonarState = HIGH;
   }
   else
   {
-    switchVal = LOW;
+    sonarState = LOW;
   }
 }
 
-void setup()
-{
-  timer.setInterval(50L, take_data);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledState);
+void pinSetup(){
+  pinMode(GND_PIN, OUTPUT);
+  pinMode(VCC_PIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+
+  digitalWrite(GND_PIN, LOW);
+  digitalWrite(VCC_PIN, HIGH);
+  digitalWrite(RELAY_PIN, HIGH);
 }
 
 void debounce()
 {
-  int reading = switchVal;
+  int reading = sonarState;
 
   if (reading != lastButtonState)
   {
@@ -64,10 +73,19 @@ void debounce()
     }
   }
 
-  digitalWrite(ledPin, ledState);
+  digitalWrite(LED_RED, !ledState);
+  digitalWrite(LED_GREEN, ledState);
+  digitalWrite(RELAY_PIN, !ledState);
 
   lastButtonState = reading;
 }
+
+void setup()
+{
+  pinSetup();
+  timer.setInterval(50L, takeData);
+}
+
 void loop()
 {
   timer.run();
